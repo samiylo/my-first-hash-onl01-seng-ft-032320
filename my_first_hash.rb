dfe457a1056fa385791db7e323e75737048f1272
Samiylo Kryshu <skryshu@gmail.com>
2020-03-26 16:09:16 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -24,7 +24,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+  
 end
 
 def adding
@@ -33,6 +33,7 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
+  shipping_manifest["oil paintings"]
   # add 2 muskets to the shipping_manifest hash below
 
 
